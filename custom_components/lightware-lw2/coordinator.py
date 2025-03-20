3a7f16213c5b15b3d278415ce1c11dccafd0a914
Kevin Schweikert <k.schweikert@im-en.com>
2025-03-20 14:57:23 +0100

    init

--- custom_components/lightware-lw2/coordinator.py
+++ custom_components/lightware-lw2/coordinator.py
@@ -3,7 +3,6 @@ from datetime import timedelta
 import logging
 import random
 
-from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers.update_coordinator import DataUpdateCoordinator
 
